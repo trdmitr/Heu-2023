bf7f94df6839343922aefdc1be7245af8f636538
trdmitr <trdmitr@gmail.com>
2023-07-27 01:05:09 +0400

    Updates

---
+++ precache-manifest.fc7e96cd9ec044ef9c713688e41f049c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab03e3ec0d198c5bea99",
+    "url": "/Heu-2023/static/css/main.80fc107b.chunk.css"
+  },
+  {
+    "revision": "ab03e3ec0d198c5bea99",
+    "url": "/Heu-2023/static/js/main.b2b2064b.chunk.js"
+  },
+  {
+    "revision": "5f2af3d968ff3361edf4",
+    "url": "/Heu-2023/static/js/runtime~main.1f288d77.js"
+  },
+  {
+    "revision": "073bede537fa6bf69aa8",
+    "url": "/Heu-2023/static/css/2.a866579d.chunk.css"
+  },
+  {
+    "revision": "073bede537fa6bf69aa8",
+    "url": "/Heu-2023/static/js/2.4825b547.chunk.js"
+  },
+  {
+    "revision": "326ba02ee63f8ce55f86",
+    "url": "/Heu-2023/static/js/3.82729454.chunk.js"
+  },
+  {
+    "revision": "f57c1202a8a62acee8a56622412c3e5b",
+    "url": "/Heu-2023/static/media/jakobctt_8b806cbf869f1c45daf1949a025cd519.f57c1202.woff"
+  },
+  {
+    "revision": "155b67a22bbea7043ce99576227dd7cf",
+    "url": "/Heu-2023/index.html"
+  }
+];
